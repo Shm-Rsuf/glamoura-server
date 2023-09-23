f3a5e49c6303184a6de98a39a3696e3303e1c4d9
shm_usuf <shmusuf@gmail.com>
2023-09-23 21:11:37 +0600

    user register method added

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,5 +1,7 @@
 import mongoose, { Schema, model } from 'mongoose';
 import { userType } from '../types/user.type';
+import validator from 'validator';
+import bcrypt from 'bcrypt';
 
 const userSchema = new Schema<userType>(
   {
@@ -50,6 +52,49 @@ const userSchema = new Schema<userType>(
     timestamps: true,
   }
 );
+
+/* register for user */
+userSchema.statics.register = async function (
+  name,
+  email,
+  password,
+  photoUrl,
+  address,
+  phoneNumber
+): Promise<userType> {
+  if (!name || !email || !password || !photoUrl) {
+    throw new Error('must be filled name, email, password, photoUrl');
+  }
+
+  const existingEmail = await this.findOne({ email });
+  if (existingEmail) {
+    throw new Error('email already exist');
+  }
+
+  if (!validator.isEmail(email)) {
+    throw new Error('invalid email');
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    throw new Error(
+      'password is not strong. It should be at least 8 characters, use both uppercase and lowercase letters, include at least one number, and have one special character.'
+    );
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(password, salt);
+
+  const user = await this.create({
+    name,
+    email,
+    password: hashPassword,
+    photoUrl,
+    address,
+    phoneNumber,
+  });
+
+  return user;
+};
 
 const UserModel = model<userType>('User', userSchema);
 export default UserModel;

9fcba7b17e9b467c8f82ff7830348977d5feee79
shm_usuf <shmusuf@gmail.com>
2023-10-03 09:12:51 +0600

    beautyPackage controller added

--- src/routes/beautyPackage.ts
+++ src/routes/beautyPackage.ts
@@ -1,33 +1,38 @@
 import express, { Router } from 'express';
 import AuthMiddleware from '../middlewares/auth.middleware';
+import BeautyPackageController from '../controllers/beautyPackage.controller';
 const beautyPackageRouter: Router = express.Router();
 const authInstance = new AuthMiddleware();
+const beautyPackageInstance = new BeautyPackageController();
 
 //get all beautyPackage
-beautyPackageRouter.get('/');
+beautyPackageRouter.get('/', beautyPackageInstance.getAllBeautyPackages);
 
 //get a single beautyPackage
-beautyPackageRouter.get('/:bid');
+beautyPackageRouter.get('/:bid', beautyPackageInstance.getSingleBeautyPackage);
 
 //create a beautyPackage
 beautyPackageRouter.post(
   '/',
   authInstance.isAuthenticated,
-  authInstance.isAdmin
+  authInstance.isAdmin,
+  beautyPackageInstance.createSingleBeautyPackage
 );
 
 //update a beautyPackage
 beautyPackageRouter.put(
   '/:bid',
   authInstance.isAuthenticated,
-  authInstance.isAdmin
+  authInstance.isAdmin,
+  beautyPackageInstance.updateBeautyPackage
 );
 
 //delete a beautyPackage
 beautyPackageRouter.delete(
   '/:bid',
   authInstance.isAuthenticated,
-  authInstance.isAdmin
+  authInstance.isAdmin,
+  beautyPackageInstance.deleteBeautyPackage
 );
 
 export default beautyPackageRouter;
